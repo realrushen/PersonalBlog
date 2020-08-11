0f5238f89a9b1aa6beef4cd579e6490311dc3fa7
rushen <realrushen@gmail.com>
2020-08-11 22:48:39 +0300

    added .gitignore file

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from time import time
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.text import slugify
 
 
@@ -29,14 +30,21 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(db_index=True, unique=True, blank=True)
     preview_text = models.CharField(max_length=200)
     text = models.TextField(blank=False)
+    published = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
     author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    views = models.PositiveIntegerField()
 
     def get_absolute_url(self):
         return reverse('post_details_url', kwargs={'slug': self.slug})
@@ -55,7 +63,5 @@ class Post(models.Model):
     def __str__(self):
         return self.slug
 
-    def __repr__(self):
-        return f'Post({self.id}, {self.title}, {self.slug}, {self.preview_text}, ' \
-               f'{self.created}, {self.updated},' \
-               f' {self.tags}'
+    class Meta:
+        ordering = ('-published',)
